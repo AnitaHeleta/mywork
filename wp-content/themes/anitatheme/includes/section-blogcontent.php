03726554763b63538d76951d76c780f0afe5be96
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2020-10-30 12:21:03 +0100

    Update section-blogcontent.php

--- wp-content/themes/anitatheme/includes/section-blogcontent.php
+++ wp-content/themes/anitatheme/includes/section-blogcontent.php
@@ -13,4 +13,14 @@ $name = get_the_author_meta ('last_name');
 <p>Posted by <?php echo $fname;?> <?php echo $lname;?></p>
 
 
+<?php
+$tags = get_the_tags();
+foreach($tags as $tag):?>
+
+
+<a href="<?php echo get_tag_link($tag->term_id);?>" class="badge badge-success">
+	<?php echo $tag->name?>
+
+<?php endforeach;?>
+
 <?php endwhile; else: endif;?>
