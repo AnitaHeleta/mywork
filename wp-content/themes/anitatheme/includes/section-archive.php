b2326656adb92e131a27b110d13d69f1879625ae
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2020-10-30 16:31:10 +0100

    Update section-archive.php

--- wp-content/themes/anitatheme/includes/section-archive.php
+++ wp-content/themes/anitatheme/includes/section-archive.php
@@ -2,18 +2,22 @@
 
 <div class="card mb-3">
 
-		<div class="card-body">
+		<div class="card-body d-flex justify-content-center-align-items-center">
 
-	<?php if(has_post_thumbnail()):?>
+			<?php if(has_post_thumbnail()):?>
 
-	<img src="<?php the_post_thumbnail_url('blog-small');?>" alt="<?php the_title();?>" class="img-field mb-3 img-thumbnail">
+			<img src="<?php the_post_thumbnail_url('blog-small');?>" alt="<?php the_title();?>" class="img-field mr-4 img-thumbnail">
 
-	<?php endif;?>
+			<?php endif;?>
 
-		<h3><?php the_title();?></h3>
-		<?php the_excerpt();?>
+			<div class="blog-content">
 
-		<a href="<?php the_permalink();?>" class="btn btn-success">Read more</a>
+				<h3><?php the_title();?></h3>
+				<?php the_excerpt();?>
+
+				<a href="<?php the_permalink();?>" class="btn btn-success">Read more</a>
+
+			</div>
 		</div>
 </div>
 
