de12752f5bf962284f7ba2b84947ef376719a012
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2020-11-02 02:00:48 +0100

    hero image

--- wp-content/themes/anitatheme/front-page.php
+++ wp-content/themes/anitatheme/front-page.php
@@ -1,7 +1,19 @@
+
+<header>
 <body>
 <div class="page-container">
 	<?php get_header();?>
 	<section class="page-wrap">
+		<div>
+
+				<?php 
+                $image = get_field('background_image'); 
+                if( $image ): ?>
+                        <img alt="<?php echo $image['title']; ?>" class="hero-background is-transparent" src="<?php echo $image['url']; ?>">
+        <?php endif; ?>
+
+    	</div>
+
 		<div class="container">
 			<h1><?php the_title();?></h1>
 			<?php get_template_part('includes/section', 'content');?>
@@ -11,3 +23,4 @@
 	<?php get_footer();?>
 </div>
 </body>
+</header>
--- wp-content/themes/anitatheme/functions.php
+++ wp-content/themes/anitatheme/functions.php
@@ -4,6 +4,8 @@
 
 function load_css()
 {
+    wp_enqueue_style('custom_fonts', '//fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,500;0,600;1,400&display=swap');
+
 
     wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
     wp_enqueue_style('bootstrap');
