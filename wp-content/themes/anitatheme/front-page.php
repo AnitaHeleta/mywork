287a0681dd4498fce1583d8afd700703b7811aec
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2020-11-02 23:12:54 +0100

    fix styling for page container and a bit of cleanup

--- wp-content/themes/anitatheme/front-page.php
+++ wp-content/themes/anitatheme/front-page.php
@@ -1,35 +1,22 @@
 
 <header>
 <body>
-		<div>
-
-				<?php 
-                $image = get_field('background_image'); 
-                if( $image ): ?>
-                        <img alt="<?php echo $image['title']; ?>" class="hero-background is-transparent" src="<?php echo $image['url']; ?>">
+	<div>
+		<?php 
+            $image = get_field('background_image'); 
+            if( $image ): ?>
+            	<img alt="<?php echo $image['title']; ?>" class="hero-background is-transparent" src="<?php echo $image['url']; ?>">
         <?php endif; ?>
-
-    	</div>
-
-<div class="page-container">
-	<?php get_header();?>
-	<section class="page-wrap">
-
-			<div class="container">
-				
-			<h1><?php the_title();?></h1>
-		
-			<?php get_template_part('includes/section', 'content');?>
-			
-			
-
-		</div>
-
-
-	</section>
-
-
-	<?php get_footer();?>
-</div>
+    </div>
+	<div class="page-container">
+		<?php get_header();?>
+		<section class="page-wrap">
+				<div class="container">
+					<h1><?php the_title();?></h1>
+					<?php get_template_part('includes/section', 'content');?>
+				</div>
+		</section>
+		<?php get_footer();?>
+	</div>
 </body>
 </header>
