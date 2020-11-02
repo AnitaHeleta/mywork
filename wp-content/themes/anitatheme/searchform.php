38d5c39ebb68b9d58a976440ab34091f641759f4
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2020-11-02 22:36:33 +0100

    style search

--- wp-content/themes/anitatheme/searchform.php
+++ wp-content/themes/anitatheme/searchform.php
@@ -1,9 +1,4 @@
 <form action="/"method="get">
-
-
-	<label for="search">Search</label>
 	<input type="text" name="s" id="search" value="<?php the_search_query();?>"required>
-
 	<button type="submit">Search!</button>
-
 </form>
