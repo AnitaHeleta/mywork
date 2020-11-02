0839d10bc0a3a93aa6125c5d3d8410bc16e0de63
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2020-11-02 22:40:34 +0100

    search button results

--- wp-content/themes/anitatheme/search.php
+++ wp-content/themes/anitatheme/search.php
@@ -3,7 +3,7 @@
 	<?php get_header();?>
 	<section class="page-wrap">
 		<div class="container">
-			<h1>Search Results for'<?php echo get_search_query();?>'</h1>
+			<h4>Search Results for'<?php echo get_search_query();?>'</h4>
 			<h1><?php echo single_cat_title();?></h1>
 			<?php get_template_part('includes/section', 'searchresults');?>
 			<?php previous_posts_link();?>
