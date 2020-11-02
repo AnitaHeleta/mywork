c3a11e696a9188f07e7b851bb11b5046937d189c
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2020-11-03 00:11:17 +0100

    fix bug on blogcontent when no tags

--- wp-content/themes/anitatheme/includes/section-blogcontent.php
+++ wp-content/themes/anitatheme/includes/section-blogcontent.php
@@ -1,43 +1,24 @@
 <?php if(have_posts()): while(have_posts()): the_post();?>
-
 <p><?php echo get_the_date();?>
-
 <?php the_content();?>
-
-
 <?php 
 $fname = get_the_author_meta('first_name');
 $name = get_the_author_meta ('last_name');
 ?>
-
 <p>Posted by <?php echo $fname;?> <?php echo $lname;?></p>
-
-
 <?php
 $tags = get_the_tags();
-foreach($tags as $tag):?>
-
-
-<a href="<?php echo get_tag_link($tag->term_id);?>" class="badge badge-success">
-	<?php echo $tag->name?>
-
-<?php endforeach;?>
-
-
+if($tags):
+	foreach($tags as $tag): ?>
+		<a href="<?php echo get_tag_link($tag->term_id);?>" class="badge badge-success">
+			<?php echo $tag->name?>
+<?php endforeach;endif;?>
 <?php
 $categories = get_the_category();
-foreach($categories as $cat):?>
-
+foreach($categories as $cat): ?>
 	<a href="<?php echo get_category_link($cat->term_id);?>">
-
 	<?php echo $cat->name;?>
-
 </a>
-
 <?php endforeach;?>
-
 <?php comments_template();?>
-
-
-
 <?php endwhile; else: endif;?>
